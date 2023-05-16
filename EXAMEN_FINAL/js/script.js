775cd2e09a746feb661b5be0ca7cb289dfd774b2
Gabriel Alzina Alomar <44143564+bielalzina@users.noreply.github.com>
2023-05-16 07:22:35 +0200

    v0.1

---
+++ EXAMEN_FINAL/js/script.js
@@ -0,0 +1,40 @@
+$(document).ready(function () {
+    // ESDEVENIMENTS
+
+    esdevenimentMouseEnter();
+    esdevenimentMouseLeave();
+    mostraToolTipMenu();
+});
+
+// ESDEVENIMENT MOUSEENTER()
+// Canvia css als elements amb class="opcio_menu" quan punter del
+// ratolí es situat a sobre
+
+function esdevenimentMouseEnter() {
+    $(".opcio_menu").on("mouseenter", function () {
+        $(this)
+            .css("background-color", "#fccda9")
+            .css("color", "black")
+            .css("font-weight", "bold")
+            .css("font-size", "22px")
+            .css("padding-top", "6px")
+            .css("padding-bottom", "6px");
+    });
+}
+
+// ESDEVENIMENT MOUSELEAVE()
+// Torna elements amb class="opcio_menu" a situació inicial quan punter del
+// ratolí deixa d'estar situat a sobre d'ell
+
+function esdevenimentMouseLeave() {
+    $(".opcio_menu").on("mouseleave", function () {
+        $(this).removeAttr("style");
+    });
+}
+
+function mostraToolTipMenu() {
+    $(".opcio_menu").tooltip({
+        content: "ADVENT mouseenter()",
+        track: true,
+    });
+}
